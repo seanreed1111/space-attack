313c4037fe01a0efd22faedf7d23e38098aa30b0
seanreed1111 <seanreed1111@gmail.com>
2016-10-25 10:50:58 -0400

    initial commit

---
+++ starfield_1.js
@@ -0,0 +1,5 @@
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext("2d");
+document.body.appendChild(canvas);
+canvas.width = window.innerWidth;
+caonvas.height = windown.innerHeight;
